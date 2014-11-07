a757c316e57caeffe852e435a347ce99a10e39e8
Arent <Arent@wcw-stud-145-109-0-84.wireless.uva.nl>
2014-11-07 12:49:32 +0100

    Initial Commit

---
+++ HiWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HiWorld
+//
+//  Created by Arent Stienstra on 07-11-14.
+//  Copyright (c) 2014 Arent Stienstra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HiWorld/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HiWorld
+//
+//  Created by Arent Stienstra on 07-11-14.
+//  Copyright (c) 2014 Arent Stienstra. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
