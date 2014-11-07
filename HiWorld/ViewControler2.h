bef304ee49557456335e9a5a7524733cb93c093c
Arent <Arent@wcw-stud-145-109-0-84.wireless.uva.nl>
2014-11-07 12:59:59 +0100

    Init And Finished

---
+++ HiWorld/ViewControler2.h
@@ -0,0 +1,13 @@
+//
+//  ViewControler2.h
+//  HiWorld
+//
+//  Created by Arent Stienstra on 07-11-14.
+//  Copyright (c) 2014 Arent Stienstra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControler2 : UIViewController
+
+@end
---
+++ HiWorld/ViewControler2.m
@@ -0,0 +1,23 @@
+//
+//  ViewControler2.m
+//  HiWorld
+//
+//  Created by Arent Stienstra on 07-11-14.
+//  Copyright (c) 2014 Arent Stienstra. All rights reserved.
+//
+
+#import "ViewControler2.h"
+
+@implementation ViewControler2
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
+
